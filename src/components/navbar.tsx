62141b68f98d06a549b81233fba83c68465e3a4d
Charan Ravi <charanravi.online@gmail.com>
2026-03-08 14:46:13 +0000

    updated youtube redirection

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -43,15 +43,17 @@ export default function Navbar() {
             <DockIcon key={name}>
               <Tooltip>
                 <TooltipTrigger asChild>
-                  <Link
+                  <a
                     href={social.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className={cn(
                       buttonVariants({ variant: "ghost", size: "icon" }),
                       "size-12"
                     )}
                   >
                     <social.icon className="size-4" />
-                  </Link>
+                  </a>
                 </TooltipTrigger>
                 <TooltipContent>
                   <p>{name}</p>
